d41cdc7f0409d627a7f037365284074d46ddfa07
Freeman0205 <61151465+Freeman0205@users.noreply.github.com>
2020-08-26 14:37:50 +0500

    Update script.js
    <div class="timer">
    				<div class="timer-title">Успей забронировать место со скидкой</div>
    				<div class="timer-action">
    					До конца акции осталось
    				</div>
    				<div class="timer-numbers" id="timer">
    					<span class="hours">18</span>
    					<span>:</span>
    					<span class="minutes">20</span>
    					<span>:</span>
    					<span class="seconds">11</span>
    				</div>
    			</div>
    //here HTML with timer , the classes are taken from here
    Отсюда берутся : timer по id и классы(.hours  .minutes  .seconds) по селектору.

--- script.js
+++ script.js
@@ -4,17 +4,17 @@
 
 // Timer
 
-    let deadLine = '2020-08-27';
+    let deadLine = '2021-08-27';              // here time, when timer must finish
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
             seconds = Math.floor((t/1000) % 60),
             minutes = Math.floor((t/1000/60) % 60),
             hours = Math.floor((t/(1000*60*60)));
-            //hours = Math.floor((t/(1000*60*60)) % 24),
-            //days = Math.floor((t/(1000*60*60*24));
+            //hours = Math.floor((t/(1000*60*60)) % 24),    ----if you need add days in timer
+            //days = Math.floor((t/(1000*60*60*24));        ----if you need add days in timer
 
-            return {
+            return {                    //function getTimeRemaining returns an object containing the remaining time  (возвращает объект с оставшимся временем)                     
                 'total' : t,
                 'hours' : hours,
                 'minutes' : minutes,
@@ -25,16 +25,16 @@
 
     function setClock(id, endtime) {
         let timer = document.getElementById(id),
-            hours = timer.querySelector('.hours'),
+            hours = timer.querySelector('.hours'),         
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
             timeInterval = setInterval(updateClock, 1000);
 
-        function updateClock() {
+        function updateClock() {                   
             let t = getTimeRemaining(endtime);
             
-                function addZero (num) {
-                    if (num <= 9) {
+                function addZero (num) {         //function to display time in the format 01:01:04 if the first digit is 0 (функция для показа времени  в формате 01:01:04 если первая цифра 0)
+                    if (num <= 9) {              
                         return '0' + num;
                     } else return num;
                 }
@@ -45,7 +45,7 @@
                     
                 
                     
-            if (t.total <= 0) {
+            if (t.total <= 0) {                      //if time is up, the page displays 00:00:00 (если время закончилось, на страницу выводятся 00:00:00)
                 clearInterval(timeInterval);
                 hours.textContent = '00';
                 minutes.textContent = '00';
